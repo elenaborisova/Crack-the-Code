fda1d214d639604074e4ea9ba4a591a791cb2165
Elena Borisova <elenaborrisova@gmail.com>
2021-04-29 13:57:21 +0300

    Update 06_area_of_shapes.py

--- Session 2 - Conditional Statements/06_area_of_shapes.py
+++ Session 2 - Conditional Statements/06_area_of_shapes.py
@@ -1,17 +1,18 @@
 import math
-type = input("Is the geometric figure a: 'square', 'rectangle', 'circle' or 'triangle? ").casefold()
 
-if type == 'square':
-    side = float(input("Side: "))
+type = input("Is the geometric figure a: square, rectangle, circle or triangle? ").casefold()
+
+if type == "square":
+    side = float(input())
     print(side * side)
-elif type == 'rectangle':
-    sideA = float(input("Side A: "))
-    sideB = float(input("Side B: "))
-    print(sideA * sideB)
-elif type == 'circle':
-    r = float(input("Radius: "))
+elif type == "rectangle":
+    side_a = float(input())
+    side_b = float(input())
+    print(side_a * side_b)
+elif type == "circle":
+    r = float(input())
     print(round(r * r * math.pi, 2))
-else: # 'triangle'
-    sideB = float(input("Side B: "))
-    h = float(input("h: "))
-    print((h * sideB) / 2)
+else:
+    side = float(input())
+    h = float(input())
+    print((h * side) / 2)
