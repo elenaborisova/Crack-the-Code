0ba72e5e9ad435f6bb5d2f1ac4c9d2486c37c2f7
Elena Borisova <elenaborrisova@gmail.com>
2021-05-12 10:32:23 +0300

    Update 05_grocery_strore.py

--- Session 3 - Conditional Statements part 2/05_grocery_strore.py
+++ Session 3 - Conditional Statements part 2/05_grocery_strore.py
@@ -7,48 +7,36 @@ price = 0
 if city == "Sofia":
     if product == "coffee":
         price = 0.50 * amount
-        print(price)
     elif product == "water":
         price = 0.80 * amount
-        print(price)
     elif product == "beer":
         price = 1.20 * amount
-        print(price)
     elif product == "sweets":
         price = 1.45 * amount
-        print(price)
     elif product == "peanuts":
         price = 1.60 * amount
-        print(price)
 elif city == "Plovdiv":
     if product == "coffee":
         price = 0.40 * amount
-        print(price)
     elif product == "water":
         price = 0.70 * amount
-        print(price)
     elif product == "beer":
         price = 1.15 * amount
-        print(price)
     elif product == "sweets":
         price = 1.30 * amount
-        print(price)
     elif product == "peanuts":
         price = 1.50 * amount
-        print(price)
 elif city == "Varna":
     if product == "coffee":
         price = 0.45 * amount
-        print(price)
     elif product == "water":
         price = 0.70 * amount
-        print(price)
     elif product == "beer":
         price = 1.10 * amount
-        print(price)
     elif product == "sweets":
         price = 1.35 * amount
-        print(price)
     elif product == "peanuts":
         price = 1.55 * amount
-        print(price)
+
+        
+print(price)
